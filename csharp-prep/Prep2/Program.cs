eef8145d8d1319d055b40be14ed8b03b36b0ca06
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-04-26 19:42:30 -0700

    prep 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int x = int.Parse(userInput);
+
+        string letterGrade = "";
+
+        if (x >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (x >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (x >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (x >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congrats! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, better luck next time!");
+        }
+
     }
 }
