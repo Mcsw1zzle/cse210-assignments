0e1bf97d071ffab5a83a7a32c261f776345af6d8
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-05-02 20:59:19 -0700

    lists done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int input = -1;
+
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            numbers.Add(input);
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count; 
+
+        Console.WriteLine($"The average is: {average}");
+
+        int highest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > highest)
+            {
+                highest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {highest}");
     }
 }
