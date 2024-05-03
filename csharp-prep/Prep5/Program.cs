0ff930c8e173ff93472b29966726cf4be3c0272a
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-05-02 21:23:43 -0700

    functions complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favNumber);
+
+        DisplayResult(name, squaredNumber);
     }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        static void DisplayResult(string userName, int number)
+        {
+            Console.WriteLine($"Brother {userName}, the square of your number is {number}");
+        }
 }
