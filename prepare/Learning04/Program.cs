90442175782233a618c948a02ba4cd9a2c474e16
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-06-06 20:23:49 -0700

    practice complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new Assignment("Parker McBride", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math = new MathAssignment("Parker McBride", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("Parker McBride", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
