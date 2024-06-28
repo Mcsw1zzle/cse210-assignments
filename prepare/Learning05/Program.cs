a0eb26f6cb1a70fc6a5dbe31dbbb0625691e3ba7
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-06-27 19:48:44 -0700

    shapes assignment done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("orange", 10);
+        shapes.Add(square);
+
+        Circle circle = new Circle("purple", 6); 
+        shapes.Add(circle);
+
+        Rectangle rectangle = new Rectangle("blue", 7, 9);
+        shapes.Add(rectangle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
+
 }
